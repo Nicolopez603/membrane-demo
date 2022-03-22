09f88549c1f24e96d3ed048a2499fdfe23378f87
Nicolas Lopez <nicomlopez3@gmail.com>
2022-03-22 16:18:29 -0300

    updating new config in reporter and apis spec

--- cypress/integration/apis.example.spec.js
+++ cypress/integration/apis.example.spec.js
@@ -1,6 +1,6 @@
 describe('Validating the response Using API', function () {
     it('Validating HTTP status code 200 on response', function () {
-          cy.fixture('api_response.json')
+          cy.fixture('apiResponse.json')
         .then(apiResponse =>{
         cy.request('POST', 'https://api-staging.membrane.trade/v2/login',
         { email: 'nicomlopez3@gmail.com', password: "asd1!!" }).then(
@@ -10,7 +10,7 @@ describe('Validating the response Using API', function () {
 })
 })
     it('Validating "statusCode:401" on response body', function () {
-    cy.fixture('api_response.json')
+    cy.fixture('apiResponse.json')
     .then(apiResponse =>{
     cy.request('POST', 'https://api-staging.membrane.trade/v2/login',
     { email: 'nicomlopez3@gmail.com', password: "asd1!!" }).then(
@@ -21,7 +21,7 @@ describe('Validating the response Using API', function () {
 })
 })
     it('Validating "code":401 error message', function () {
-    cy.fixture('api_response.json')
+    cy.fixture('apiResponse.json')
     .then(apiResponse =>{
     cy.request('POST', 'https://api-staging.membrane.trade/v2/login',
     { email: 'nicomlopez3@gmail.com', password: "asd1!!" }).then(
@@ -32,7 +32,7 @@ describe('Validating the response Using API', function () {
 })
 })
     it('body response should match', () => {
-    cy.fixture('api_response.json')
+    cy.fixture('apiResponse.json')
     .then(apiResponse =>{
     cy.request('POST', 'https://api-staging.membrane.trade/v2/login',
     { email: 'nicomlopez3@gmail.com', password: "asd1!!" }).then(
