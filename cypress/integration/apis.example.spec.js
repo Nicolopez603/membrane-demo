7200a889774494dd00c6df22c4cc8ba2c5906e51
Nicolas Lopez <81532585+Nicolopez603@users.noreply.github.com>
2022-03-24 21:28:41 -0300

    improving indentation in the api code

--- cypress/integration/apis.example.spec.js
+++ cypress/integration/apis.example.spec.js
@@ -1,45 +1,47 @@
-describe('Validating the response Using API', function () {
-    it('Validating HTTP status code 200 on response', function () {
-          cy.fixture('apiResponse.json')
-        .then(apiResponse =>{
-        cy.request('POST', 'https://api-staging.membrane.trade/v2/login',
-        { email: 'nicomlopez3@gmail.com', password: "asd1!!" }).then(
-         (response) => {
+describe("Validating the response Using API", function () {
+  it("Validating HTTP status code 200 on response", function () {
+    cy.fixture("apiResponse.json").then((apiResponse) => {
+      cy.request("POST", "https://api-staging.membrane.trade/v2/login", {
+        email: "nicomlopez3@gmail.com",
+        password: "asd1!!",
+      }).then((response) => {
         expect(response.status).to.deep.equal(apiResponse.statusCode);
+      });
+    });
+  });
+  it('Validating "statusCode:401" on response body', function () {
+    cy.fixture("apiResponse.json").then((apiResponse) => {
+      cy.request("POST", "https://api-staging.membrane.trade/v2/login", {
+        email: "nicomlopez3@gmail.com",
+        password: "asd1!!",
+      }).then((response) => {
+        expect(response.body.statusCode).to.deep.equal(
+          apiResponse.body.statusCode
+        );
+        cy.log(apiResponse);
+      });
+    });
+  });
+  it('Validating "code":401 error message', function () {
+    cy.fixture("apiResponse.json").then((apiResponse) => {
+      cy.request("POST", "https://api-staging.membrane.trade/v2/login", {
+        email: "nicomlopez3@gmail.com",
+        password: "asd1!!",
+      }).then((response) => {
+        expect(response.body.error.code).to.deep.equal(
+          apiResponse.body.error.code
+        );
+      });
+    });
+  });
+  it("body response should match", () => {
+    cy.fixture("apiResponse.json").then((apiResponse) => {
+      cy.request("POST", "https://api-staging.membrane.trade/v2/login", {
+        email: "nicomlopez3@gmail.com",
+        password: "asd1!!",
+      }).then((response) => {
+        expect(response.body).to.deep.equal(apiResponse.body);
+      });
+    });
+  });
 });
-})
-})
-    it('Validating "statusCode:401" on response body', function () {
-    cy.fixture('apiResponse.json')
-    .then(apiResponse =>{
-    cy.request('POST', 'https://api-staging.membrane.trade/v2/login',
-    { email: 'nicomlopez3@gmail.com', password: "asd1!!" }).then(
-    (response) => {
-    expect(response.body.statusCode).to.deep.equal(apiResponse.body.statusCode);
-    cy.log(apiResponse);
-});
-})
-})
-    it('Validating "code":401 error message', function () {
-    cy.fixture('apiResponse.json')
-    .then(apiResponse =>{
-    cy.request('POST', 'https://api-staging.membrane.trade/v2/login',
-    { email: 'nicomlopez3@gmail.com', password: "asd1!!" }).then(
-     (response) => {
-    expect(response.body.error.code).to.deep.equal(apiResponse.body.error.code);
-
-});
-})
-})
-    it('body response should match', () => {
-    cy.fixture('apiResponse.json')
-    .then(apiResponse =>{
-    cy.request('POST', 'https://api-staging.membrane.trade/v2/login',
-    { email: 'nicomlopez3@gmail.com', password: "asd1!!" }).then(
-    (response) => {
-    expect(response.body).to.deep.equal(apiResponse.body);
-
-})
-});
-})
-})
